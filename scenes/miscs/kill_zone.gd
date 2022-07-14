b686866d6a122695156f33b3599344bd1501953f
davzeng <48802653+davzeng@users.noreply.github.com>
2022-07-13 20:08:24 -0700

    Fixed spore + death issue

--- scenes/miscs/kill_zone.gd
+++ scenes/miscs/kill_zone.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
 var player :Node
+var hit:int = 0
 
 func _ready():#pause node and sets player
 	player = get_tree().current_scene._get_player()
 
 func _physics_process(_delta):
+	if hit > 0:
+		hit -= 1
+		return
 	if overlaps_body(player):
 		if not player.warp:
 			player.dead = true
+			hit = 5
